63a74390ba313b67dfa452bc7bc04bf00ee1cd1f
nazrulahmed <nazrulahmed.se@gmail.com>
2022-01-21 18:46:52 +0600

    AddTwoNumber - start

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
+}
+
+public class AddTwoNumbers {
+
+    /*
+                  Solution Approach:
+
+
+     */
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ListNode l1 = new ListNode();
+
+    }
+
+}
+
+
